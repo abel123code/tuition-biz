3692ef601d61bd5ede007faef0e97b4cdc704c05
Abel Lee <binghengabellee@gmail.com>
2024-09-19 12:32:12 +0800

    changed pdf viewer

--- frontend/src/component/PDFReader.jsx
+++ frontend/src/component/PDFReader.jsx
@@ -1,21 +1,15 @@
 import React from 'react';
-
 const PDFReader = ({ contentURL }) => {
   // Appending #toolbar=0 to disable the PDF toolbar (no download or print)
   const pdfURL = `${contentURL}#toolbar=0`;
-
   return (
     <iframe
       src={pdfURL}
       title="PDF Viewer"
-      className="w-full h-full"
-      style={{
-        border: 'none',
-        width: '100%',
-        height: '100%',
-      }}
+      width="100%"
+      height="100%"
+      style={{ border: 'none' }}
     />
   );
 };
-
 export default PDFReader;
